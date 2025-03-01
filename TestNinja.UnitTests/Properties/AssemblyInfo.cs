ee4eaafb8158531560fb4d1fb70318bd944e1b78
leylaheydarova <lbadalzade@gmail.com>
2025-03-02 01:37:18 +0400

    Reservation test was done for the first time

---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b667767-c789-40ab-a799-37dcaa467dee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests //Naming principle is name of the class which we test its methods + "Tests"
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue() //Naming principle is name of tested method + scenario(condition) + behavior(what should do).
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true }); //act is to apply method itself with the argument which is set in scenario. (admin)
+
+            //Assert
+            Assert.IsTrue(result); //one way of assert syntax
+        }
+
+        [Test]
+        public void CanbeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            reservation.MadeBy = new User { IsAdmin = false };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(reservation.MadeBy);
+
+            //Assert
+            Assert.That(result, Is.True); //second way of assert syntax (more readable)
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            //Assert
+            Assert.That(result == false); //third way of assert syntax
+        }
+    }
+}
