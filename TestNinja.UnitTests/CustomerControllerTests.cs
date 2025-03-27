7f1aa48026644a2d03f3b4780de2e207e3ee9997
leylaheydarova <lbadalzade@gmail.com>
2025-03-28 00:51:53 +0400

    Testing Void Methods, with Exceptions, Events

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new CustomerController();
+        }
+        [Test]
+        public void GetCustomer_WhenIdIsZero_ShouldReturnNotFound()
+        {
+            var result = _controller.GetCustomer(0);
+            //There are two ways of assertion of object: TypeOf and InstanceOf. The difference between them is,
+            //TypeOf checks if result is NotFound object itself, concurrently;
+            //However InstanceOf checks if result is NotFound itself or one of its derivatives objects. (base class)
+            Assert.That(result, Is.TypeOf<NotFound>()); //Only NotFound
+            //Assert.That(result, Is.InstanceOf<NotFound>()); //Notfound and its derivatives
+        }
+
+        [Test]
+        public void GetCustomer_WhenIdIsNotZero_ShouldReturnOk()
+        {
+            var result = _controller.GetCustomer(1);
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
--- TestNinja/Fundamentals/ErrorLogger.cs
+++ TestNinja/Fundamentals/ErrorLogger.cs
@@ -5,21 +5,21 @@ namespace TestNinja.Fundamentals
 {
     public class ErrorLogger
     {
-        public string LastError { get; set; }
+        public string LastError { get; private set; }
 
-        public event EventHandler<Guid> ErrorLogged; 
-        
+        public event EventHandler<Guid> ErrorLogged;
+
         public void Log(string error)
         {
             if (String.IsNullOrWhiteSpace(error))
-                throw new ArgumentNullException();
-                
-            LastError = error; 
-            
-            // Write the log to a storage
+                throw new ArgumentNullException(); //testing throw exception case
+
+            LastError = error; //testing major logic of method
+
+            //Write the log to a storage
             // ...
 
-            ErrorLogged?.Invoke(this, Guid.NewGuid());
+            ErrorLogged?.Invoke(this, Guid.NewGuid()); //testing event
         }
     }
 }
