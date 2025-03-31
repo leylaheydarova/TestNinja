302ddca892e5801d58eb8fb434640b1532c4ca41
leylaheydarova <lbadalzade@gmail.com>
2025-04-01 02:53:02 +0400

    Stack task was done

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_WhenIsDividedByThreeAndFive_ShouldReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Does.Contain("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenIsDividedByThree_ShouldReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Does.EndWith("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenIsDividedByFive_ShouldReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Does.StartWith("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenIsDividedByOtherNumber_ShouldReturnNumberInString()
+        {
+            var number = 4;
+            var result = FizzBuzz.GetOutput(number);
+            Assert.That(result, Is.EqualTo(number.ToString()));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Stack<string>();
+        }
+
+        [Test]
+        public void Push_WhenObjIsNull_ShouldThrowArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ObjIsNotNull_ShouldAddObject()
+        {
+            _stack.Push("Leyla");
+            Assert.That(_stack.Count, Is.Not.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_WhenCountIsZero_ShouldThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_WhenCountIsNotZero_ShouldRemoveLastObj()
+        {
+            _stack.Push("Zohrab");
+            _stack.Push("Kamile");
+            var currentCount = _stack.Count;
+            _stack.Pop();
+            Assert.That(_stack.Count, Is.EqualTo(currentCount - 1));
+        }
+
+        [Test]
+        public void Pop_WhenCountIsNotZero_ShouldReturnLastObj()
+        {
+            _stack.Push("Zohrab");
+            _stack.Push("Kamile");
+            var currentCount = _stack.Count;
+            var result = _stack.Pop();
+            Assert.That(result, Is.EqualTo("kamile").IgnoreCase);
+        }
+
+        [Test]
+        public void Peek_WhenCountIsZero_ShouldThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_WhenCountIsNotZero_ShouldReturnLastObject()
+        {
+            _stack.Push("Ruslan");
+            _stack.Push("Sona");
+            var result = _stack.Peek();
+            Assert.That(result, Does.Contain("Sona"));
+        }
+    }
+}
