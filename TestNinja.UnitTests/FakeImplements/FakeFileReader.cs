0523d671310e18bc60cbe87201e56bde7588b177
leylaheydarova <lbadalzade@gmail.com>
2025-04-18 02:21:10 +0400

    Mocking FileReader

---
+++ TestNinja.UnitTests/FakeImplements/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.FakeImplements
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string ReadFile(string path)
+        {
+            return "";
+        }
+    }
+}
